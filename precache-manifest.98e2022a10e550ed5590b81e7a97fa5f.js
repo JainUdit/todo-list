e10b6d66524b29ef3d1509a579d9bb8566d85ae1
Udit Jain <jain_udit@infogain.com>
2020-09-27 21:55:00 +0530

    Updates

---
+++ precache-manifest.98e2022a10e550ed5590b81e7a97fa5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3a850b4c6224e9e29ee0b7256485046",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "60789b4eeba8d34ff479",
+    "url": "/todo-list/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "19aee827a67671b9badf",
+    "url": "/todo-list/static/js/2.7e0af1f0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todo-list/static/js/2.7e0af1f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60789b4eeba8d34ff479",
+    "url": "/todo-list/static/js/main.372fb142.chunk.js"
+  },
+  {
+    "revision": "a557b42a7f266985029e",
+    "url": "/todo-list/static/js/runtime-main.9c8ee466.js"
+  }
+]);
